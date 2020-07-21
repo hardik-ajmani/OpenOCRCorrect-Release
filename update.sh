6634fe9254e00f42ba4ef242235c131040adfcac
Nipun Ramani <ramaninipun@gmail.com>
2020-07-21 21:49:04 +0530

    fixed update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-make clean
+sudo apt full-upgrade
+sudo apt-get install qt5-default
+sudo apt-get tesseract-ocr
 git pull origin corrector-version
+cd FrameWorkCode
 qmake qpadfinal.pro
+make clean
 make
